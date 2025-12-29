e80adc1b852ada7d058493dba4d7acfc3a51a0f9
siwol025 <cyk99115695@gmail.com>
2025-12-29 13:26:14 +0900

    chore: initialize project

---
+++ src/main/java/com/siwol025/flight_monitor/FlightMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.siwol025.flight_monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siwol025/flight_monitor/FlightMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siwol025.flight_monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
